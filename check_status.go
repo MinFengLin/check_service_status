b54db9bc5d9fd566ce67fad38df78d9fcc15497d
Minfeng <minfenglin0406@gmail.com>
2022-10-18 07:47:12 +0000

    v1.0.1
    - ADD: Support crontab

--- check_status.go
+++ check_status.go
@@ -14,11 +14,27 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 )
 
 var (
 	service_data service.Services_slice
 )
+
+func check(chatid *int64, token *string) {
+	service_data = service.Parser_services()
+	failed_data := ""
+	for ii := range service_data.Services {
+		service.Check_service_status(ii, 500, &service_data, &failed_data)
+	}
+	if len(failed_data) > 0 {
+		failed_data = "↻ Check Status ...... 🔴FAILED! \n - \n" + failed_data + "-"
+		bot_service.Telegram_bot_run(*chatid, *token, failed_data)
+	} else {
+		failed_data = "↻ Check Status ...... 🟢PASS! \n" + failed_data
+		bot_service.Telegram_bot_run(*chatid, *token, failed_data)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -29,23 +45,24 @@ func main() {
 	chatID, _   := strconv.ParseInt(os.Getenv("chatID"), 10, 64)
 	yourToken   := os.Getenv("yourToken")
 	Env_Time, _ := strconv.ParseInt(os.Getenv("Timer_Minutes"), 10, 64)
+	crontab_time := os.Getenv("Crontime")
 
-	fmt.Printf("chatID: %d, yourToken: %s, How often to run: %d (Minutes)\n", chatID, yourToken, Env_Time)
-	tChannel := time.NewTimer(time.Duration(Env_Time) * time.Minute)
-	for {
-		service_data = service.Parser_services()
-		failed_data := ""
-		for ii := range service_data.Services {
-			service.Check_service_status(ii, 500, &service_data, &failed_data)
-		}
-		if len(failed_data) > 0 {
-			failed_data = "↻ Check Status ...... 🔴FAILED! \n - \n" + failed_data + "-"
-			bot_service.Telegram_bot_run(chatID, yourToken, failed_data)
-		} else {
-			failed_data = "↻ Check Status ...... 🟢PASS! \n" + failed_data
-			bot_service.Telegram_bot_run(chatID, yourToken, failed_data)
-		}
-		tChannel.Reset(time.Duration(Env_Time) * time.Minute)
-		<-tChannel.C
-	 }
+	if len(crontab_time) > 0 {
+		fmt.Printf("chatID: %d, yourToken: %s, Use crontab: %s \n", chatID, yourToken, crontab_time)
+		c := cron.New()
+		c.AddFunc(crontab_time, func() {
+			check(&chatID, &yourToken)
+		})
+		check(&chatID, &yourToken)
+		c.Start()
+		select{}
+	} else {
+		fmt.Printf("chatID: %d, yourToken: %s, How often to run: %d (Minutes)\n", chatID, yourToken, Env_Time)
+		tChannel := time.NewTimer(time.Duration(Env_Time) * time.Minute)
+		for {
+			check(&chatID, &yourToken)
+			tChannel.Reset(time.Duration(Env_Time) * time.Minute)
+			<-tChannel.C
+		 }
+	}
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ go 1.19
 require (
 	bot v0.0.0-00010101000000-000000000000
 	github.com/joho/godotenv v1.4.0
+	github.com/robfig/cron/v3 v3.0.1
 	service v0.0.0-00010101000000-000000000000
 )
 
